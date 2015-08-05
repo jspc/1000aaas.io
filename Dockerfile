cc0989fac42dcf79a2cf76df7059b79b9c102082
jspc <james@zero-internet.org.uk>
2015-08-05 14:51:36 +0100

    Populate repo

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM scratch
+COPY main /main
+EXPOSE 8000
+CMD ["/main"]
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "bytes"
+    "fmt"
+    "net/http"
+    "io/ioutil"
+
+    "github.com/zenazn/goji"
+    "github.com/zenazn/goji/web"
+)
+
+var a bytes.Buffer
+var response string
+var url string
+
+func get_a(c web.C, w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, a.String())
+}
+
+func proxy (c web.C, w http.ResponseWriter, r *http.Request) {
+   switch c.URLParams["proxy"] {
+    default:
+        url = "http://jelly.stupids.org:6565"
+    }
+
+    req, err := http.NewRequest("Get", url, nil)
+    if err != nil {
+        response = err.Error()
+    } else {
+        defer req.Body.Close()
+        body, err := ioutil.ReadAll(req.Body)
+        if err != nil {
+            response = err.Error()
+        } else {
+            response = string(body[:])
+        }
+    }
+
+    fmt.Fprint(w, response)
+}
+
+func main() {
+    for i := 0; i < 1000; i++ {
+        a.WriteString("a")
+    }
+
+    goji.Get("/v1/a", get_a)
+    goji.Get("/v1/proxy/:proxy", proxy)
+    goji.Serve()
+}
