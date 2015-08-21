862805e95860a256bb01516b1f0eba9586de1428
jspc <james@zero-internet.org.uk>
2015-08-21 15:48:15 +0100

    Remove/rename cruft

--- main.go
+++ main.go
@@ -19,29 +19,7 @@ func get_a(c web.C, w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, a.String())
 }
 
-func proxy (c web.C, w http.ResponseWriter, r *http.Request) {
-   switch c.URLParams["proxy"] {
-    default:
-        url = "http://jelly.stupids.org:6565"
-    }
-
-    req, err := http.NewRequest("Get", url, nil)
-    if err != nil {
-        response = err.Error()
-    } else {
-        defer req.Body.Close()
-        body, err := ioutil.ReadAll(req.Body)
-        if err != nil {
-            response = err.Error()
-        } else {
-            response = string(body[:])
-        }
-    }
-
-    fmt.Fprint(w, response)
-}
-
-func healthcheck(c web.C, w http.ResponseWriter, r *http.Request) {
+func get_root(c web.C, w http.ResponseWriter, r *http.Request) {
     name, _ := os.Hostname()
     fmt.Fprintf(w, "Healthy! host: %s", name)
 }
@@ -51,8 +29,7 @@ func main() {
         a.WriteString("a")
     }
 
-    goji.Get("/", healthcheck)
+    goji.Get("/", get_root)
     goji.Get("/v1/a", get_a)
-    goji.Get("/v1/proxy/:proxy", proxy)
     goji.Serve()
 }
